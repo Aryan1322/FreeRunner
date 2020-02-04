ec7c204b7c09584be4d9e676a5409fc744b0c816
Aryan1322 <Aryan1322@users.noreply.github.com>
2020-02-04 19:03:13 +1100

    Add Player and obstical
    add Player movment and obstcal spawn

---
+++ Assets/Obsticals.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticals : MonoBehaviour
+{
+    public GameObject fireball;
+    public float shootingSpeed;
+    public Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnFireBall();
+        }
+    }
+    void SpawnFireBall()
+    {
+        GameObject fireballProject = Instantiate(fireball, transform.position, Quaternion.identity) as GameObject;
+      
+        rb2D = fireballProject.GetComponent<Rigidbody2D>();
+        rb2D.AddForce(Vector3.left * shootingSpeed * Time.deltaTime);
+        
+    }
+}
