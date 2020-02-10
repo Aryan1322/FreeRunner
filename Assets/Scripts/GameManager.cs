cfdad334b459fbca412bd97f098f1c4132b805c7
Aryan1322 <60641308+Aryan1322@users.noreply.github.com>
2020-02-10 16:24:06 +1100

    add score and fix
    i add the platfrom to play at it

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float Score;
+    public Text textScore;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score += 2 * Time.deltaTime;
+
+        if(Score >= 500)
+        {
+            //spawn Platform
+
+
+        }
+        if(Score >= 1500)
+        {
+            //despawn the platfrom on the sides
+        }
+        textScore.text = Score.ToString();
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -28,4 +28,11 @@ public class Movement : MonoBehaviour
         rb2D.AddForce(transform.up * gravity);
 
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("You Lose");
+        }
+    }
 }
